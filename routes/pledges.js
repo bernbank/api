69b47711cb922219971d24a49fb9025d43ced988
jimsimon <jim.j.simon@gmail.com>
2016-04-12 22:38:08 -0400

    Refactored to use promises instead of callbacks

--- routes/pledges.js
+++ routes/pledges.js
@@ -7,17 +7,20 @@ var pledgeService = new PledgeService(url);
 
 router.post('/', function(req, res){
   pledgeService.createPledge(req.body)
-      .then((pledge) => res.json(pledge));
+      .then((pledge) => res.json(pledge))
+      .catch((e) => res.status(500).send(e));
 });
 
 router.get('/:id', function(req, res) {
   pledgeService.getPledge(req.params.id)
-      .then((pledge) => res.json(pledge));
+      .then((pledge) => res.json(pledge))
+      .catch((e) => res.status(500).send(e));
 });
 
 router.delete('/:id', function(req, res) {
   pledgeService.deletePledge(req.params.id)
-      .then(() => res.status(204).send());
+      .then(() => res.status(204).send())
+      .catch((e) => res.status(500).send(e));
 });
 
 module.exports = router;
--- services/pledge-service.js
+++ services/pledge-service.js
@@ -8,39 +8,49 @@ class PledgeService {
     constructor(url) {
         this.url = url;
     }
+
     createPledge(pledge) {
         return new Promise((resolve, reject) => {
-            MongoClient.connect(this.url, function(err, db){
+            MongoClient.connect(this.url).then(function(db){
                 var pledges = db.collection('pledges');
-                pledges.insertOne(pledge, function(err, record){
-                    db.close();
+                pledges.insertOne(pledge).then(function(record){
                     resolve(record.ops[0]);
+                    db.close();
+                }).catch((err) => {
+                    reject(err);
+                    db.close();
                 });
-            });
+            }).catch(reject);
         });
     }
 
     getPledge(id) {
         return new Promise((resolve, reject) => {
-            MongoClient.connect(this.url, function (err, db) {
+            MongoClient.connect(this.url).then(function (db) {
                 var pledges = db.collection('pledges');
-                pledges.find({"_id": new ObjectId(id)}).limit(1).next(function(err, doc){
-                    db.close();
+                pledges.find({"_id": new ObjectId(id)}).limit(1).next().then(function(doc){
                     resolve(doc);
+                    db.close();
+                }).catch((err) => {
+                    reject(err);
+                    db.close();
                 });
-            });
+            }).catch(reject);
         });
     }
 
     deletePledge(id) {
         return new Promise((resolve, reject) => {
-            MongoClient.connect(this.url, function (err, db) {
+            MongoClient.connect(this.url).then(function (db) {
                 var pledges = db.collection('pledges');
-                pledges.deleteOne({"_id": new ObjectId(id)}, function(err, result){
-                    db.close();
+                pledges.deleteOne({"_id": new ObjectId(id)}).then(function(result){
                     resolve();
+                    db.close();
+                }).catch((err) => {
+                    reject(err);
+                    db.close();
                 });
-            });
+            }).catch(reject);
         });
     }
 }
