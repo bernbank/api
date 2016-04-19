7bc555d84e3e794ba132d3e0751ea669254454da
jimsimon <jim.j.simon@gmail.com>
2016-04-18 23:04:59 -0400

    Refactored job code into separate file and adjusted save format for call log

--- bin/bernbank.js
+++ bin/bernbank.js
@@ -2,36 +2,9 @@
 var process = require('process');
 var app = require('../app');
 var http = require('http');
-var moment = require('moment');
-var CronJob = require('cron').CronJob;
-var config = require('../config/config');
-var MongoCache = require('../util/mongo-cache');
-var BerniePbClient = require('../api_clients/bernie-pb-client');
-var DailyCallLogService = require('../services/daily-call-log-service');
+var jobs = require('./jobs');
 
-new CronJob('00 00 12 * * *', function () {
-    var mongoCache = new MongoCache();
-    mongoCache.getDb(config.mongo.connectionString).then((db) => {
-        var berniePbClient = new BerniePbClient();
-        var yesterday = moment().subtract(1, 'day').format('YYYY-MM-DD');
-        berniePbClient.getCallersAboveThresholdByDate(yesterday, config.callThreshold).then((callers) => {
-            console.log('Successfully fetched callers above threshold by date');
-            var dailyCallLogService = new DailyCallLogService(db);
-            dailyCallLogService.saveDailyCallLog(callers).then(() => {
-                console.log('Saved daily call log');
-            }).catch((err) => {
-                console.error('Failed to save daily call log');
-                console.error(err.stack);
-            })
-        }).catch((err) => {
-            console.error('Failed to fetch callers above threshold by date');
-            console.error(err.stack);
-        });
-    }).catch((err) => {
-        console.error('Failed to get mongo db connection');
-        console.error(err.stack);
-    });
-}, null, true, 'America/Detroit', null, true);
+jobs.setupJobs();
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 3000);
