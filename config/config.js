529cb14b8d66285759e0114d25d64f033842a268
alvaro <zephyr.3@gmail.com>
2016-04-27 17:18:30 -0700

    Added node-cache module to system to speed the retrieval of information.

--- config/config.js
+++ config/config.js
@@ -4,5 +4,6 @@ module.exports = {
     mongo: {
         connectionString: 'mongodb://localhost:27017/berniebank'
     },
-    callThreshold: 50
+    callThreshold: 50,
+    nodeCache: { stdTTL: 600, checkperiod: 605 } ,
 };
