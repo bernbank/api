d17a4aff430867de1eb7d7cad11096fa6e7c03d6
alvaro <zephyr.3@gmail.com>
2016-05-04 13:38:52 -0700

    Removed tabs and added spaces instead in services/mailing-service.js

--- services/mailing-service.js
+++ services/mailing-service.js
@@ -11,80 +11,80 @@ class MailingService {
         this.mailinglist = db.collection('mailinglist');
     }
 
-	/**
-	 * Gets a list of all active emails
-	 **/
-	getActiveEmails() {
-		return new Promise((resolve, reject) => {
-			var query = {active:true};
-			this.mailinglist.find(query, { email: true }, (err, thing) => {				
-				if (err != null) {
-					 // There was an error, let's report it
-					reject(e);
-				}
-				
-				var output = [];
-				thing.each( (err,doc) => {
-					
-					if (err != null) {
-						 // There was an error, let's report it
-						reject(err);
-					}
+    /**
+     * Gets a list of all active emails
+     **/
+    getActiveEmails() {
+        return new Promise((resolve, reject) => {
+            var query = {active:true};
+            this.mailinglist.find(query, { email: true }, (err, thing) => {                
+                if (err != null) {
+                     // There was an error, let's report it
+                    reject(e);
+                }
+                
+                var output = [];
+                thing.each( (err,doc) => {
+                    
+                    if (err != null) {
+                         // There was an error, let's report it
+                        reject(err);
+                    }
 
-					if (doc != null) {
-						output.push(doc);
-					} else {
-						resolve(output);
-					}
-				});
-						
-			});
-			
-		});
-		
-	}
-
-
-	/**
-	 * (Soft) deletes en email from the mailinglist collection
-	 **/
-	deleteEmail(strEmail) {
-		return new Promise((resolve, reject) => {
-			var query = {email: strEmail};
-			
-			this.mailinglist.update(query, {"$set" : {active: false} }, (err, results) => {
-				if (err != null) {
-					reject(err);
-				} else {
-					resolve();
-				}
-			});			
-		});
-	}
+                    if (doc != null) {
+                        output.push(doc);
+                    } else {
+                        resolve(output);
+                    }
+                });
+                        
+            });
+            
+        });
+        
+    }
 
 
-	/**
-	 * (Soft) deletes en email from the mailinglist collection
-	 **/
-	insertEmails(emails) {
-		return new Promise((resolve, reject) => {
-			var bulk = this.mailinglist.initializeUnorderedBulkOp();
+    /**
+     * (Soft) deletes en email from the mailinglist collection
+     **/
+    deleteEmail(strEmail) {
+        return new Promise((resolve, reject) => {
+            var query = {email: strEmail};
+            
+            this.mailinglist.update(query, {"$set" : {active: false} }, (err, results) => {
+                if (err != null) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });            
+        });
+    }
 
-			async.each(emails, (email, callback) => {
-				email["active"] = true;
-				bulk.insert(email);
-				callback();
-			}, 
-			(err) => {
 
-				bulk.execute((err, result) => {
-					resolve();
-				});
-			
-			});
-			
-		});
-	}
+    /**
+     * (Soft) deletes en email from the mailinglist collection
+     **/
+    insertEmails(emails) {
+        return new Promise((resolve, reject) => {
+            var bulk = this.mailinglist.initializeUnorderedBulkOp();
+
+            async.each(emails, (email, callback) => {
+                email["active"] = true;
+                bulk.insert(email);
+                callback();
+            }, 
+            (err) => {
+
+                bulk.execute((err, result) => {
+                    resolve();
+                });
+            
+            });
+            
+        });
+    }
 
 
     /**
@@ -115,34 +115,34 @@ class MailingService {
         return new Promise( (resolve, reject) => {
             var client = ses.createClient(config.amazonSES);
 
-			if (strDryRunEmail == "") {
-				// Find all available emails and send emails to those guys :)
-				var query = {active:true};
-				this.mailinglist.find(query, (err, thing) => {				
-					if (err != null) {
-						 // There was an error, let's report it
-						reject(e);
-					}
-					thing.each( (err,doc) => {
-						if (err != null) {
-							 // There was an error, let's report it
-							reject(err);
-						}
-						if (doc != null) {
-							this.sendSingleEmail(client,  doc);
-						} else {
-							resolve();
-						}
-					});		
-				});
-			} else {
-				this.sendSingleEmail(client,  {
-					firstname: 'Dry',
-					lastname : 'Runner',
-					email: strDryRunEmail
-				});
-				resolve();
-			}
+            if (strDryRunEmail == "") {
+                // Find all available emails and send emails to those guys :)
+                var query = {active:true};
+                this.mailinglist.find(query, (err, thing) => {                
+                    if (err != null) {
+                         // There was an error, let's report it
+                        reject(e);
+                    }
+                    thing.each( (err,doc) => {
+                        if (err != null) {
+                             // There was an error, let's report it
+                            reject(err);
+                        }
+                        if (doc != null) {
+                            this.sendSingleEmail(client,  doc);
+                        } else {
+                            resolve();
+                        }
+                    });        
+                });
+            } else {
+                this.sendSingleEmail(client,  {
+                    firstname: 'Dry',
+                    lastname : 'Runner',
+                    email: strDryRunEmail
+                });
+                resolve();
+            }
 
 
         });
