a6794636e9615bd724208c52fcbed9d35556c082
alvaro <zephyr.3@gmail.com>
2016-05-10 16:16:18 -0700

    Changed threadshold to 50.

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     mongo: {
         connectionString: 'mongodb://localhost:27017/berniebank'
     },
-    callThreshold: 30,
+    callThreshold: 50,
     nodeCache: { stdTTL: 600, checkperiod: 605 } ,
     amazonSES: { key: 'key', secret: 'secret' }
 };
