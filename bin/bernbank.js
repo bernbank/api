5a4035c90be024060bb302644a2a4e9acc5c85e2
jimsimon <jim.j.simon@gmail.com>
2016-06-23 17:08:07 -0400

    Disabled cron jobs now that primary season is over

--- bin/bernbank.js
+++ bin/bernbank.js
@@ -2,9 +2,11 @@
 var process = require('process');
 var app = require('../app');
 var http = require('http');
-var jobs = require('./jobs');
 
-jobs.setupJobs();
+//Disabled jobs now that primary season is over
+//var jobs = require('./jobs');
+//
+//jobs.setupJobs();
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 3000);
